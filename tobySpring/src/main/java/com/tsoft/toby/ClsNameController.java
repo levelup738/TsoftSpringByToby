c9f4f97e44656b4918b8227adcfd18ebd7edd4a7
kwontae94 <a1046117304@gmail.com>
2022-09-07 00:26:25 +0900

    권태민 : 핸들러 mapping 정리

---
+++ tobySpring/src/main/java/com/tsoft/toby/ClsNameController.java
@@ -0,0 +1,34 @@
+package com.tsoft.toby;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ClsNameController implements Controller {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ClsNameController.class);
+
+	@Autowired
+	HelloSpring helloSpring;
+
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String name = req.getParameter("name");
+		String message = this.helloSpring.sayHello(name);
+
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("message", message);
+		
+		logger.info("ClsNameController : {}", message);
+
+		return new ModelAndView("hello", model);
+	}
+
+}
---
+++ tobySpring/src/main/java/com/tsoft/toby/MyController.java
@@ -0,0 +1,36 @@
+package com.tsoft.toby;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("my")
+public class MyController implements Controller {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyController.class);
+
+	@Autowired
+	HelloSpring helloSpring;
+	
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String name = req.getParameter("name");
+		String message = this.helloSpring.sayHello(name);
+
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("message", message);
+		
+		logger.info("MyController : {}", message);
+
+		return new ModelAndView("my", model);
+	}
+
+}
